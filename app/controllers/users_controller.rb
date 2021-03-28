392f282e6ea67e52cda4489de5841fdb92c6d2be
Ömer Hodo <omerhodo@gmail.com>
2021-03-28 10:26:46 +0300

    some problem fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
     def new
         @user=User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          flash[:notice] = "Your account information was successfully updated"
+          redirect_to articles_path
+        else
+          render 'edit'
+        end
+    end
+
     def create
-        @user=User.create(user_params)
+        @user = User.create(user_params)
         if @user.save
             flash[:notice]="Welcome to the Alpa Blog #{@user.username}"
             redirect_to articles_path
