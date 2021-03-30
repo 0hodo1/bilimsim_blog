63a4be9ce2f525ce52651e876b953c78191b4999
Ömer Hodo <omerhodo@gmail.com>
2021-03-30 14:17:01 +0300

    user and article require added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -14,14 +16,12 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
     
     def update
-        @user = User.find(params[:id])
         if @user.update
-          flash[:notice] = "Your account information was successfully updated"
-          redirect_to @user
+            flash[:notice] = "Your account information was successfully updated"
+            redirect_to @user
         else
           render 'edit'
         end
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice]="Welcome to the Alpa Blog #{@user.username}"
             redirect_to articles_path
         else
@@ -42,4 +43,16 @@ class UsersController < ApplicationController
         def user_params
             params.require(:user).permit(:username, :email, :password)
         end
+
+        def set_user
+            @user = User.find(params[:id])
+        end
+
+        def require_same_user
+            if  @user!=current_user
+                flash[:alert]="You can only edit your own account"
+                redirect_to @user
+            end
+        end
+
 end
